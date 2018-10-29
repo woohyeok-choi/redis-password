ae1eb78d99c88ae8ea72bda9c6acded4a01570d1
Woohyeok Choi <keltpower0@gmail.com>
2018-10-29 21:42:41 +0900

    v 2.0

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-SECRETS_FILE="/run/secrets/${SECRETS}"
+SECRET_FILE="/run/secrets/${SECRET_NAME}"
 
-if [ -f ${SECRETS_FILE} ]; then
-    REDIS_PASSWORD=$(crudini --get ${SECRETS_FILE} redis password)
+if [ -z ${INI_SECTION} ]; then
+    INI_SECTION=''
 fi
+if [ -f ${SECRET_FILE} ]; then
+    REDIS_PASSWORD=$(crudini --get ${SECRET_FILE} "${INI_SECTION}" password)
+fi
+
+
 
 if [ -n ${REDIS_PASSWORD} ]; then
 cat<<EOF >> /etc/redis/conf.d/redis-server.conf
